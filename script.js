8de4910ebce262ededaedec4e2c98931deb3d1e2
AASHIRWAD KUMAR <aashirwadkumar159@gmail.com>
2022-07-04 23:32:51 +0530

    responsivene style updated  links given as output

--- script.js
+++ script.js
@@ -13,20 +13,38 @@
     const reader = new FileReader();
     reader.onload = function fileReadCompleted() {
      
-      console.log(reader.result);
+      // console.log(reader.result);
       var lines = reader.result.split('\n');
 
       var outputContainer = document.getElementById("file-output");
+      var outputContainernonClickable = document.getElementById("file-output-nonclickable");
       lines.forEach(function(element, index){
        
-        if(element.includes('http')){
-            var elementForOutput = document.createElement('p');
-            elementForOutput.innerText = element;
-            outputContainer.appendChild(elementForOutput);
+        var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+
+        if(element.includes('http') && !(element.includes('meet'))){
+          // console.log((element.indexOf(" ",[, (element.indexOf("http"))])))
+          console.log(element.match(urlRegex))
+          // console.log(element.substring(element.indexOf("http")))
+          var regText=element.match(urlRegex)[0];
+         
+          var paraForOutput=document.createElement('p')
+            var elementForOutput = document.createElement('a');
+            elementForOutput.href=regText;
+            elementForOutput.innerHTML = regText;
+            paraForOutput.appendChild(elementForOutput)
+            outputContainer.appendChild(paraForOutput);
+
+            // var elementfornonclickable=document.createElement('a')
+            // elementfornonclickable.innerHTML=regText;
+            // var parafornonclickable=document.createElement('p')
+            // parafornonclickable.appendChild(elementfornonclickable)
+            
+            // outputContainernonClickable.appendChild(parafornonclickable)
 
         }
         else{
-            console.log("ee")
+            // console.log("ee")
         }
         
      });
